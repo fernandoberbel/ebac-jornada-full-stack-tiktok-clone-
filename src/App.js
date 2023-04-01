c54503ce73c4d07702c52a248b67fed20872c4a6
fberbel <fberbel@gmail.com>
2023-04-01 00:46:54 -0300

    Add Sidebar

--- src/App.js
+++ src/App.js
@@ -6,11 +6,24 @@ function App() {
   return (
     <div className="App">
       <div className='app__videos'>
-        <Video />
-        <Video />
-        <Video />
-        <Video />
-        <Video />
+        <Video
+          likes={111}
+          messages={222}
+          shares={333}
+          name="Paulo"
+          description="Brecker o goleiro"
+          music="Música épica"
+          url="https://poqlymuephttfsljdabn.supabase.co/storage/v1/object/public/jornadadev/brecker2.mp4"
+        />
+        <Video
+          likes={111}
+          messages={222}
+          shares={333}
+          name="Pedro"
+          description="Bird olhando para a camera"
+          music="Clap your hands"
+          url="https://poqlymuephttfsljdabn.supabase.co/storage/v1/object/public/jornadadev/bird.mp4"
+        />
       </div>
     </div>
   );
